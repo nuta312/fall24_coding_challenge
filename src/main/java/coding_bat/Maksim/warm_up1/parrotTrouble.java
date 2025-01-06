b6532187605b747df2c2e644e30dda00b65a36a0
Wringer <192597421+Wringers@users.noreply.github.com>
2025-01-06 16:02:07 +0600

    monkeyTrouble

---
+++ src/main/java/coding_bat/Maksim/warm_up1/parrotTrouble.java
@@ -0,0 +1,7 @@
+package coding_bat.Maksim.warm_up1;
+
+public class parrotTrouble {
+    public boolean parrotTrouble(boolean talking, int hour) {
+    return ((hour < 7 || hour > 20)&&talking);
+}
+}
