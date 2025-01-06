e4541896aec421af561f2630496aa361e3d68322
Bulat Black <bulatblack@MacBook-Air-Bulat.local>
2025-01-07 02:49:21 +0600

    kuba/warmup1-In3050

---
+++ src/main/java/coding_bat/kuba/warm_up1/In3050.java
@@ -0,0 +1,9 @@
+package coding_bat.kuba.warm_up1;
+
+public class In3050 {
+    public boolean in3050(int a, int b) {
+        if ((a >= 30 && b >= 30) && (a <= 40 && b <= 40) || ((a >= 40 && b >= 40) && (a <= 50 && b <= 50))) {
+            return true;
+        } return false;
+    }
+}
