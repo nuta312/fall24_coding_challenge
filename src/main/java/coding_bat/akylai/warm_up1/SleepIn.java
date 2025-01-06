f36681d9685d9fd96b19cdd1aa4e03a43e20405d
Rano <ranotoktorbaeva@gmail.com>
2025-01-06 11:31:09 +0600

    rano/warmup1-SleepIn

--- src/main/java/coding_bat/akylai/warm_up1/SleepIn.java
+++
@@ -1,7 +0,0 @@
-package coding_bat.akylai.warm_up1;
-
-public class SleepIn {
-    public boolean sleepIn(boolean weekday, boolean vacation) {
-        return !weekday || vacation;
-    }
-}
--- src/main/java/coding_bat/amir/warm_up1/SleepIn.java
+++
@@ -1,8 +0,0 @@
-package coding_bat.amir.warm_up1;
-
-public class SleepIn {
-    public boolean sleepIn(boolean weekday, boolean vacation) {
-        return !weekday  || vacation;
-
-    }
-}
