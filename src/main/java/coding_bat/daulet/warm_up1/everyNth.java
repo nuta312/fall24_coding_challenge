0763eb5f57bd1a1a2b042bf3acbdb8afdf194e6a
Пользователь <192666536+Daulerto@users.noreply.github.com>
2025-01-06 20:18:41 +0600

    daulet/warm_up1/everyNth

---
+++ src/main/java/coding_bat/daulet/warm_up1/everyNth.java
@@ -0,0 +1,13 @@
+package coding_bat.daulet.warm_up1;
+
+public class everyNth {
+    public String everyNth(String str, int n) {
+        String result = "";
+
+        // Look at every nth char
+        for (int i=0; i<str.length(); i = i + n) {
+            result = result + str.charAt(i);
+        }
+        return result;
+    }
+}
