98f57d4a6bc016fc135cb73fb92a32ccf5c28d03
Elnura <elnura.dogdurbekova@gmail.com>
2025-01-06 16:49:34 +0600

    SleepIn

--- src/main/java/coding_bat/delnura/warm_up1/SleepIn.java
+++ src/main/java/coding_bat/delnura/warm_up1/SleepIn.java
@@ -1,4 +1,13 @@
 package coding_bat.delnura.warm_up1;
 
 public class SleepIn {
+
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation) {
+            return true;
+        }
+
+        return false;
+    }
+
 }
