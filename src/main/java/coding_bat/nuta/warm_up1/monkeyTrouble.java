c5049d789aa28bd018eee43a2e9b9e0fd60e770e
Wringer <192597421+Wringers@users.noreply.github.com>
2025-01-06 11:03:12 +0600

    monkeyTrouble

---
+++ src/main/java/coding_bat/nuta/warm_up1/monkeyTrouble.java
@@ -0,0 +1,13 @@
+package coding_bat.nuta.warm_up1;
+
+public class monkeyTrouble {
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if (aSmile && bSmile) {
+            return true;
+        }
+        if (!aSmile && !bSmile) {
+            return true;}
+        return false;
+
+    }
+}
