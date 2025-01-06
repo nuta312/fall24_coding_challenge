d464d6cbbc136f0bc3e05c5395ecbd04fac6cee9
Aliyusha31 <aliyaizabekova4@gmail.com>
2025-01-07 01:09:08 +0600

    Delete src/main/java/coding_bat/alia_warmup1/Sleep_in.java

--- src/main/java/coding_bat/alia_warmup1/Sleep_in.java
+++
@@ -1,9 +0,0 @@
-package coding_bat.alia_warmup1;
-
-public class Sleep_in {
-    public boolean sleepIn(boolean weekday, boolean vacation) {
-        if (!weekday || vacation){
-            return true;
-        }return false;
-    }
-}
