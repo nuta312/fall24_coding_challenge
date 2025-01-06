a09a8175ad62139c6d7dda31d340e4aba1a917fa
Nurda <nurdan.kazakbaev@gmail.com>
2025-01-06 17:28:16 +0600

    nurdan/warm_up1/SumDouble

--- src/main/java/coding_bat/nurdan/warm_up1/SumDouble.java
+++ src/main/java/coding_bat/nurdan/warm_up1/SumDouble.java
@@ -1,4 +1,16 @@
 package coding_bat.nurdan.warm_up1;
 
 public class SumDouble {
+    public int sumDouble(int a, int b) {
+        // Store the sum in a local variable
+        int sum = a + b;
+
+        // Double it if a and b are the same
+        if (a == b) {
+            sum = sum * 2;
+            return sum;
+        }
+
+        else return sum;
+    }
 }
