80fe2fdb74716ef623bffcbba9fa7f051183287b
Rinat Zhanaliev <rinatjanaliev@MacBook-Pro-Rinat.local>
2025-01-06 17:21:43 +0600

    rinat/warmup1:Diff21

---
+++ src/main/java/coding_bat/rinat/warm_up1/Diff21.java
@@ -0,0 +1,12 @@
+package coding_bat.rinat.warm_up1;
+
+public class Diff21 {
+    public int diff21(int n) {
+        if (n <= 21){
+            return 21-n;
+        } else {
+            return (n - 21) * 2;
+
+        }
+    }
+}
