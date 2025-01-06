32ef1ae37a08f058037f98ab4bf4cf5e20bdf659
Nurlan Seitov <seitov@gmail.com>
2025-01-07 01:15:07 +0600

    Создана ветка Nurlans/warm_up2, добавлен класс sumDouble

---
+++ src/main/java/coding_bat/Nurlans/warm_up2/sumDouble.java
@@ -0,0 +1,9 @@
+package coding_bat.Nurlans.warm_up2;
+
+public class sumDouble {
+    public int sumDouble(int a, int b) {
+        if (a>b||a<b)
+        {return a+b;}
+        else {return (a+b)*2;}
+    }
+}
