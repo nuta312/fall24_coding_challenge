3146258cb70a0a416f48d0b4c3deb7e3549bcc48
Aliya <aliyaizabekova4@gmail.com>
2025-01-07 00:24:42 +0600

    Add Sleep_in.java implementation

---
+++ src/main/java/coding_bat/alia_warmup1/Sleep_in.java
@@ -0,0 +1,10 @@
+package coding_bat.alia_warmup1;
+
+public class Sleep_in {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation){
+            return true;
+        }return false;
+    }
+
+}
