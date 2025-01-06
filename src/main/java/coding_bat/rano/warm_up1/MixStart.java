7715fc615c9db31e55871578dd496fdc4ae2e99b
Rano <ranotoktorbaeva@gmail.com>
2025-01-06 22:20:31 +0600

    rano/warmup1-MixStart

---
+++ src/main/java/coding_bat/rano/warm_up1/MixStart.java
@@ -0,0 +1,11 @@
+package coding_bat.rano.warm_up1;
+
+public class MixStart {
+    public boolean mixStart(String str) {
+        if (str.startsWith("ix", 1))
+            return true;
+        else
+            return false;
+
+    }
+}
