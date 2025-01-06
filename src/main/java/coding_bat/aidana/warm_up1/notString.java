20d336a9f331754a8cfd3b7b888df647280aec99
Aidana <aidana.shamirbekkyzy.ch22@gmail.com>
2025-01-07 00:11:33 +0600

    aidana commit not String

---
+++ src/main/java/coding_bat/aidana/warm_up1/notString.java
@@ -0,0 +1,11 @@
+package coding_bat.aidana.warm_up1;
+
+public class notString {
+    public String notString(String str) {
+        if (str.startsWith("not")){
+            return  str;
+        } else {
+            return "not " + str;
+        }
+    }
+}
