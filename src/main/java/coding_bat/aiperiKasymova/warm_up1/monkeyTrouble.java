58cc8a44be2e790467ee4bafca2f3c41a3e539c0
Aiperi <aibige24wka@gmail.com>
2025-01-06 20:55:46 +0600

    aiperiKasymova/warm_up1/monkeyTrouble

--- src/main/java/coding_bat/aiperiKasymova/warm_up1/monkeyTrouble.java
+++ src/main/java/coding_bat/aiperiKasymova/warm_up1/monkeyTrouble.java
@@ -1,2 +1,11 @@
-package coding_bat.aiperiKasymova.warm_up1;public class monkeyTrouble {
+package coding_bat.aiperiKasymova.warm_up1;
+
+public class monkeyTrouble {
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if (aSmile && bSmile)
+            return true;
+        if(!aSmile && !bSmile)
+            return true;
+        return false;
+    }
 }
