9ed6b7e1895a4a97686456c23f074df45e1807e2
Aiperi <aibige24wka@gmail.com>
2025-01-06 16:46:28 +0600

    aiperiKasymova/warm_up1-SleepIn

---
+++ src/main/java/coding_bat/aiperiKasymova/warm_up1/SleepIn.java
@@ -0,0 +1,7 @@
+package coding_bat.aiperiKasymova.warm_up1;
+
+public class SleepIn {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return  (!weekday || vacation);
+    }
+}
