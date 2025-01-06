0d356887b90efe0b62e02f2e538cf18b95fbf5c9
ChelovekBayan <58964760+ChelovekBayan@users.noreply.github.com>
2025-01-06 11:58:57 +0600

    amir/warmup_1.sumDouble

--- src/main/java/coding_bat/amir/warm_up1/MonkeyTrouble.java
+++ src/main/java/coding_bat/amir/warm_up1/MonkeyTrouble.java
@@ -1,4 +1,8 @@
 package coding_bat.amir.warm_up1;
 
 public class MonkeyTrouble {
+    public boolean monkeyTrouble(boolean a, boolean b) {
+        return a == b;
+    }
+
 }
---
+++ src/main/java/coding_bat/amir/warm_up1/SumDouble.java
@@ -0,0 +1,7 @@
+package coding_bat.amir.warm_up1;
+
+public class SumDouble {
+    public int sumDouble(int a, int b) {
+        return a == b ? (a+b)*2 : a+b;
+    }
+}
