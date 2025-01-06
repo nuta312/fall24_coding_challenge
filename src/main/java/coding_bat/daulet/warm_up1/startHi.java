a2a507bb9fc7f7a3b5164a7aa4387d65d8c3a5ef
Пользователь <192666536+Daulerto@users.noreply.github.com>
2025-01-06 19:42:01 +0600

    daulet/warm_up1/startHi

---
+++ src/main/java/coding_bat/daulet/warm_up1/startHi.java
@@ -0,0 +1,9 @@
+package coding_bat.daulet.warm_up1;
+
+public class startHi {
+    public boolean startHi(String str) {
+        if(str.startsWith("hi")){
+            return true;
+        }return false;
+    }
+}
