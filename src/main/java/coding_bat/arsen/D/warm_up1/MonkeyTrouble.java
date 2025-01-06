98f34a48fc5191c924b6bca4b311637e6dd59461
Максим <192597421+wringers@users.noreply.github.com>
2025-01-06 13:39:21 +0600

    решение задачи monkeyTrouble

---
+++ src/main/java/coding_bat/arsen/D/warm_up1/MonkeyTrouble.java
@@ -0,0 +1,10 @@
+package coding_bat.arsen.D.warm_up1;
+
+public class MonkeyTrouble {
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+    if(aSmile == bSmile){
+        return true;
+    }
+    return false;
+}
+}
