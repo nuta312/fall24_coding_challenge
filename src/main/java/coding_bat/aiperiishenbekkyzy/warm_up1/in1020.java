f3c022182357b4d0e6b7d1a19e5c8db86e756e59
Aiperi <ishenbekovaaiperi@gmail.com>
2025-01-06 21:46:47 +0600

    aiperiishenbekkyzy/warm_up1-in1020

--- src/main/java/coding_bat/aiperiishenbekkyzy/warm_up1/in1020.java
+++ src/main/java/coding_bat/aiperiishenbekkyzy/warm_up1/in1020.java
@@ -1,5 +1,7 @@
 package coding_bat.aiperiishenbekkyzy.warm_up1;
 
 public class in1020 {
-
+    public boolean in1020(int a, int b) {
+        return ((a >= 10 && a <= 20) || (b >=10 && b <=20));
+    }
 }
