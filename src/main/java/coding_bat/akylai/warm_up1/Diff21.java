ea2c10b5184094a3bdea1d96532332723b55b6ba
Akylai <192575286+Akylai89@users.noreply.github.com>
2025-01-06 14:19:56 +0600

    Akylai/warm_up1_Diff21

---
+++ src/main/java/coding_bat/akylai/warm_up1/Diff21.java
@@ -0,0 +1,12 @@
+package coding_bat.akylai.warm_up1;
+
+public class Diff21 {
+    public int diff21(int n) {
+        if (n > 21) {
+            return (n - 21) * 2;
+        } else if (n <= 21) {
+            return 21 - n;
+        }
+        return n;
+    }
+}
