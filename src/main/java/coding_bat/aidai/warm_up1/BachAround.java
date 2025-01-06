d804f73d0e7f15cc96aa444957b57f4ff1018872
Aidai <aidaiurkalieva7@gmail.com>
2025-01-07 03:49:50 +0600

    aidai/warm-up-backAround

---
+++ src/main/java/coding_bat/aidai/warm_up1/BachAround.java
@@ -0,0 +1,9 @@
+package coding_bat.aidai.warm_up1;
+
+public class BachAround {
+    public String backAround(String str) {
+        // Get the last char
+        String last = str.substring(str.length() - 1);
+        return last + str + last;
+    }gid
+}
