a3b4e8aa147a1cfc0881045884e875b5f5a48148
Nurda <nurdan.kazakbaev@gmail.com>
2025-01-06 19:50:38 +0600

    nurdan/makes10

--- src/main/java/coding_bat/nurdan/warm_up1/makes10.java
+++ src/main/java/coding_bat/nurdan/warm_up1/makes10.java
@@ -1,4 +1,10 @@
 package coding_bat.nurdan.warm_up1;
 
 public class makes10 {
+    public boolean makes10(int a, int b) {
+        if (a == 10 || b ==10 || a + b == 10){
+            return true;
+        } return false;
+    }
+
 }
