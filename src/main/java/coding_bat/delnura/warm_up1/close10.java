690a023e7e2d173ad8dcb2c3b10068416cc540bf
Elnura <elnura.dogdurbekova@gmail.com>
2025-01-07 00:33:55 +0600

    delnura/warm_up1-close10

---
+++ src/main/java/coding_bat/delnura/warm_up1/close10.java
@@ -0,0 +1,16 @@
+package coding_bat.delnura.warm_up1;
+
+public class close10 {
+    public int close10(int a, int b) {
+        int aDiff = Math.abs(a - 10);
+        int bDiff = Math.abs(b - 10);
+
+        if (aDiff < bDiff) {
+            return a;
+        }
+        if (bDiff < aDiff) {
+            return b;
+        }
+        return 0;
+    }
+}
