3cdb722f046339f4c740b827ee73e0fa9bc396aa
Пользователь <192666536+Daulerto@users.noreply.github.com>
2025-01-06 23:15:04 +0600

    daulet/warm_up2/stringTimes

---
+++ src/main/java/coding_bat/daulet/warm_up2/stringTimes.java
@@ -0,0 +1,11 @@
+package coding_bat.daulet.warm_up2;
+
+public class stringTimes {
+    public String stringTimes(String str, int n) {
+        String result = "";
+        for (int i=0; i<n; i++) {
+            result = result + str;  // could use += here
+        }
+        return result;
+    }
+}
