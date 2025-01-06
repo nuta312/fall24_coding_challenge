0f7ad6bed81c7d5973ba126bdbb02235fd3afcf5
aygerim <aygerim.kasymova.94@internet.ru>
2025-01-06 22:54:17 +0600

    everyNth commit

---
+++ src/main/java/coding_bat/aygerim_warm_up1/everyNth.java
@@ -0,0 +1,12 @@
+package coding_bat.aygerim_warm_up1;
+
+public class everyNth {
+    public String everyNth(String str, int n) {
+        String result = "";
+        for (int i = 0; i < str.length();i = i + n) {
+            result = result + str.charAt(i);
+        }
+        return result;
+    }
+
+}
