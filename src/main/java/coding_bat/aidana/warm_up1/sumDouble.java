0185c40e38dd4e4437f221aea2bb96c166cdca99
Aidana <aidana.shamirbekkyzy.ch22@gmail.com>
2025-01-06 22:54:00 +0600

    aidana.warm_up1 sumDouble

---
+++ src/main/java/coding_bat/aidana/warm_up1/sumDouble.java
@@ -0,0 +1,11 @@
+package coding_bat.aidana.warm_up1;
+
+public class sumDouble {
+    public int sumDouble(int a, int b) {
+        if (a == b){
+            return (a + b) * 2;
+        } else {
+            return a + b;
+        }
+    }
+}
