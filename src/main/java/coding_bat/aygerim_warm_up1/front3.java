10bd747526bb15538bcfe413003e17958dfd8e16
aygerim <aygerim.kasymova.94@internet.ru>
2025-01-07 00:01:10 +0600

    front3 commit

---
+++ src/main/java/coding_bat/aygerim_warm_up1/front3.java
@@ -0,0 +1,17 @@
+package coding_bat.aygerim_warm_up1;
+
+public class front3 {
+    public String front3(String str) {
+        String front;
+        if (str.length() >=3) {
+            front = str.substring(0,3);
+
+        }
+        else {
+            front = str;
+
+        }
+        return front + front + front;
+    }
+
+}
