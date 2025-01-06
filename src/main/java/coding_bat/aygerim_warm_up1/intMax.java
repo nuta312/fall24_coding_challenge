078f2b5ff876a6961efdddcd426114a875d3ffb1
aygerim <aygerim.kasymova.94@internet.ru>
2025-01-07 00:22:14 +0600

    intMax commit

---
+++ src/main/java/coding_bat/aygerim_warm_up1/intMax.java
@@ -0,0 +1,19 @@
+package coding_bat.aygerim_warm_up1;
+
+public class intMax {
+    public int intMax(int a, int b, int c) {
+        int max;
+        if (a > b) {
+            max = a;
+
+        } else {
+            max = b;
+
+        }
+        if (c > max) {
+            max = c;
+        }
+        return max;
+    }
+
+}
