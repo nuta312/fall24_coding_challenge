fac9e4a2096efe3bddbfd076beac40875063af3f
Пользователь <192666536+Daulerto@users.noreply.github.com>
2025-01-06 23:30:20 +0600

    daulet/warm_up2/stringYak

---
+++ src/main/java/coding_bat/daulet/warm_up2/stringYak.java
@@ -0,0 +1,10 @@
+package coding_bat.daulet.warm_up2;
+
+public class stringYak {
+    public String stringYak(String str) {
+        if (str.contains("yak")){
+            return str.replaceAll("yak","");
+        }return str;
+    }
+
+}
