e1b90b238efb591b5e23b9f59a0be5e5c38f7737
Ars472 <arsendautov6@gmail.com>
2025-01-06 14:39:12 +0600

    Arsen.D решение задачи NearHundred

---
+++ src/main/java/coding_bat/arsen/D/warm_up1/NearHundred.java
@@ -0,0 +1,8 @@
+package coding_bat.arsen.D.warm_up1;
+
+public class NearHundred {
+    public boolean nearHundred(int n) {
+
+        return ((Math.abs(100 - n) <= 10)||(Math.abs(200 - n) <=10)) ;
+    }
+}
