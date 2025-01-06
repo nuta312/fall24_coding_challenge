0c403cc0a2acd4d95ef4b7e876686e00d7a97683
Elnura <elnura.dogdurbekova@gmail.com>
2025-01-07 00:39:51 +0600

    delnura/warm_up1-stringE

---
+++ src/main/java/coding_bat/delnura/warm_up1/stringE.java
@@ -0,0 +1,16 @@
+package coding_bat.delnura.warm_up1;
+
+public class stringE {
+    public boolean stringE(String str) {
+        int count = 0;
+
+        for (int i=0; i<str.length(); i++) {
+            if (str.charAt(i) == 'e') count++;
+
+        }
+
+        return (count >= 1 && count <= 3);
+
+
+    }
+}
