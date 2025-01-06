583dcdb6975e4fbe6e2778b31816b753b1b3bf22
Mirat <98100116+migabitov@users.noreply.github.com>
2025-01-06 16:10:31 +0600

    nearHundred

---
+++ src/main/java/coding_bat/mirat/warm_up1/NearHundred.java
@@ -0,0 +1,9 @@
+package coding_bat.mirat.warm_up1;
+
+public class NearHundred {
+    public boolean nearHundred(int n) {
+
+        return (Math.abs(n - 100) <= 10) || (Math.abs(n - 200) <= 10);
+    }
+
+}
