04508b335d2e7ee9f3ed126e8346f4cd27d34d72
Максим <192597421+Wringers@users.noreply.github.com>
2025-01-06 18:43:50 +0600

    Revert "Rano/warmup1 sum double"

--- src/main/java/coding_bat/aidai/warm_up1/SleepIn.java
+++ src/main/java/coding_bat/aidai/warm_up1/SleepIn.java
@@ -1,12 +1,9 @@
 package coding_bat.aidai.warm_up1;
+
 public class SleepIn {
     public boolean sleepIn(boolean weekday, boolean vacation) {
-        if (weekday) {
-            return vacation;
-        }
-        return true;
+        return !weekday || vacation;
     }
 }
-
 
 
