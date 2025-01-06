8ce6c5ba3a8737738728cf4196019cedc2c354a4
Aidai <aidaiurkalieva7@gmail.com>
2025-01-07 03:43:05 +0600

    aidai/warm-up-MissingChar

---
+++ src/main/java/coding_bat/aidai/warm_up1/MissChar.java
@@ -0,0 +1,12 @@
+package coding_bat.aidai.warm_up1;
+
+public class MissChar {
+    public String missingChar(String str, int n) {
+        String front = str.substring(0, n);
+
+
+        String back = str.substring(n+1, str.length());
+
+        return front + back;
+    }
+}
