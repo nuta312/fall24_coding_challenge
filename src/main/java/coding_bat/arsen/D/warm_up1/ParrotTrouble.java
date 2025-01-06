132d15be6d4ea677559409e9c67d3d663d74d9e9
Ars472 <arsendautov6@gmail.com>
2025-01-06 14:10:01 +0600

    Arsen.D решение задачи ParrotTrouble

---
+++ src/main/java/coding_bat/arsen/D/warm_up1/ParrotTrouble.java
@@ -0,0 +1,9 @@
+package coding_bat.arsen.D.warm_up1;
+
+public class ParrotTrouble {
+    public boolean parrotTrouble(boolean talking, int hour) {
+
+        return (talking && (hour <7 || hour > 20));
+    }
+
+}
