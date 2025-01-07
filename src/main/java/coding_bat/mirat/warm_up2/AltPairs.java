083acfd8dd7d4f92c1ae8ee94cb435bf3ffb57d4
Mirat <98100116+migabitov@users.noreply.github.com>
2025-01-07 23:04:57 +0600

    Объединенный коммит: все изменения.
    Объединенный коммит: все изменения.

---
+++ src/main/java/coding_bat/mirat/warm_up2/AltPairs.java
@@ -0,0 +1,18 @@
+package coding_bat.mirat.warm_up2;
+
+public class AltPairs {
+    public String altPairs(String str) {
+    String result = "";
+
+    for (int i=0; i<str.length(); i += 4) {
+        int end = i + 2;
+        if (end > str.length()) {
+            end = str.length();
+        }
+        result = result + str.substring(i, end);
+    }
+
+    return result;
+}
+
+}
