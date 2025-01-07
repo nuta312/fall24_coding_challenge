4b3884c84f6aff1ca6b481d9041c1c41235d6a43
Mirat <98100116+migabitov@users.noreply.github.com>
2025-01-07 19:12:25 +0600

    EveryNth

---
+++ src/main/java/coding_bat/mirat/warm_up1/EveryNth.java
@@ -0,0 +1,13 @@
+package coding_bat.mirat.warm_up1;
+
+public class EveryNth {
+    public String everyNth(String str, int n) {
+        String result = "";
+
+        // Look at every nth char
+        for (int i=0; i<str.length(); i = i + n) {
+            result = result + str.charAt(i);
+        }
+        return result;
+    }
+}
