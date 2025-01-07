52551b8d8bf32bf2de0cef4ef75f30409e895293
Rinat Zhanaliev <rinatjanaliev@MacBook-Pro-Rinat.local>
2025-01-07 18:39:17 +0600

    rinat/warmup2:arrayCount9

---
+++ src/main/java/coding_bat/rinat/warm_up2/ArrayCount9.java
@@ -0,0 +1,15 @@
+package coding_bat.rinat.warm_up2;
+
+public class ArrayCount9 {
+    public int arrayCount9(int[] nums) {
+        int count = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 9)
+                count++;
+        }
+
+        return count;
+
+    }
+}
