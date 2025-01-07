8b7bd777dd73541e1764a82282e777308c6a7ca8
Anara Alymbaeva <boardmemberbt@gmail.com>
2025-01-07 11:29:17 +0300

    anara/warm_up1/parrotTrouble

---
+++ src/main/java/coding_bat/anara/warm_up1/parrotTrouble.java
@@ -0,0 +1,10 @@
+package coding_bat.anara.warm_up1;
+
+public class parrotTrouble {
+    public boolean parrotTrouble(boolean talking, int hour) {
+        if (hour >= 7 && hour <= 20) {
+            return false;
+        } return talking;
+    }
+
+}
