bc813d0d92894e87a01dbc62ec449ea568a2f91a
Ars472 <arsendautov6@gmail.com>
2025-01-07 14:16:51 +0600

    Arsen.D решение задачи StringTimes

---
+++ src/main/java/coding_bat/arsen/D/warm_up2/StringTimes.java
@@ -0,0 +1,14 @@
+package coding_bat.arsen.D.warm_up2;
+
+public class StringTimes {
+
+    public String stringTimes(String str, int n) {
+        String result = "";
+        for (int i=0; i<n; i++) {
+            result = result + str;  // could use += here
+        }
+        return result;
+
+    }
+
+}
