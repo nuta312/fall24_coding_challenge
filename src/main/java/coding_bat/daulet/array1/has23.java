835892561e90413211b0e12503278ae21d84d26a
Пользователь <192666536+Daulerto@users.noreply.github.com>
2025-01-07 22:07:44 +0600

    "daulet/array1/allTasksInOneCommit" # This is a combination of 27 commits.
    daulet/array1/firstLast6
    
    daulet/array1/commonEnd
    
    daulet/array1/reverse3
    
    daulet/array1/middleWay
    
    daulet/array1/no23
    
    daulet/array1/fix23
    
    daulet/array1/makeMiddle
    
    daulet/array1/midThree
    
    daulet/array1/unlucky1
    
    daulet/array1/sameFirstLast
    
    daulet/array1/sum3
    
    daulet/array1/maxEnd3
    
    daulet/array1/makeEnds
    
    daulet/array1/makeLast
    
    daulet/array1/start1
    
    daulet/array1/plusTwo
    
    daulet/array1/maxTriple
    
    daulet/array1/make2
    
    daulet/array1/makePi
    
    daulet/array1/rotateLeft3
    
    daulet/array1/sum2
    
    daulet/array1/has23
    
    daulet/array1/double23
    
    daulet/array1/biggerTwo
    
    daulet/array1/swapEnds
    
    daulet/array1/frontPiece
    
    daulet/array1/front11

---
+++ src/main/java/coding_bat/daulet/array1/has23.java
@@ -0,0 +1,11 @@
+package coding_bat.daulet.array1;
+
+public class has23 {
+    public boolean has23(int[] nums) {
+        if (nums[0]==2 || nums[1]==2 || nums[0]==3 || nums[1]==3){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
