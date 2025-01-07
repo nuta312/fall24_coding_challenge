fd0e4abaf5ae068502dac3cd7b9b43716f9fefd9
Adinai <adika.kenen.2005@gmail.com>
2025-01-07 18:49:40 +0600

    adinai/warm_up1-everyNth

---
+++ src/main/java/coding_bat/adinai/warm_up1/EveryNth.java
@@ -0,0 +1,11 @@
+package coding_bat.adinai.warm_up1;
+
+public class EveryNth {
+    public String everyNth(String str, int n) {
+        String result = "";
+        for (int i=0; i<str.length(); i=i+n) {
+            result = result + str.charAt(i);
+        }
+        return result;
+    }
+}
