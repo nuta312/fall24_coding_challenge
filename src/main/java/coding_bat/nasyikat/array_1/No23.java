d27ca67866b0a3880d2854dd234906da5dc89f81
Nasyikat <Nasi_94@mail.ru>
2025-01-07 20:24:36 +0600

    Array-1 all one commit
    nasyikat/Array-1/firstLast6
    
    nasyikat/Array-1/sameFirstLast
    
    nasyikat/Array-1/makePi
    
    nasyikat/Array-1/commonEnd
    
    nasyikat/Array-1/sum3
    
    nasyikat/Array-1/rotateLeft3
    
    nasyikat/Array-1/reverse3
    
    nasyikat/Array-1/maxEnd3
    
    nasyikat/Array-1/sum2
    
    nasyikat/Array-1/middleWay
    
    nasyikat/Array-1/makeEnds
    
    nasyikat/Array-1/has23
    
    nasyikat/Array-1/no23
    
    nasyikat/Array-1/makeLast
    
    nasyikat/Array-1/double23

---
+++ src/main/java/coding_bat/nasyikat/array_1/No23.java
@@ -0,0 +1,10 @@
+package coding_bat.nasyikat.array_1;
+
+public class No23 {
+    public boolean no23(int[] nums) {
+        if (nums[0] != 2 && nums[0] != 3 && nums[1] != 2 && nums[1] != 3){
+            return true;
+        }
+        return false;
+    }
+}
