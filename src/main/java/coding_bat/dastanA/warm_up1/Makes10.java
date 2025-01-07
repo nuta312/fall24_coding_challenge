b21e640f975a2e5084aca37255d52973270d78c9
adastanious <dastanaidarov1@gmail.com>
2025-01-07 22:04:35 +0600

    dastanA-warm-up-1-Makes10

---
+++ src/main/java/coding_bat/dastanA/warm_up1/Makes10.java
@@ -0,0 +1,10 @@
+package coding_bat.dastanA.warm_up1;
+
+public class Makes10 {
+    public boolean makes10(int a, int b) {
+        if (a == 10 || b == 10 || a+b == 10){
+            return true;
+        }
+        return false;
+    }
+}
