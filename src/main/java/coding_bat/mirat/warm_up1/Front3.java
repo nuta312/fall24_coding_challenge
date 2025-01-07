7ea3b606dad639e6af80ccdfbbb6b76f57732f92
Mirat <98100116+migabitov@users.noreply.github.com>
2025-01-07 19:00:23 +0600

    BackAround

--- src/main/java/coding_bat/mirat/warm_up1/Front3.java
+++ src/main/java/coding_bat/mirat/warm_up1/Front3.java
@@ -1,4 +1,14 @@
 package coding_bat.mirat.warm_up1;
 
 public class Front3 {
+    public String front3(String str) {
+        if (str.length() > 3){
+            str = str.substring(0,3);
+            return str+str+str;
+        }
+        else{
+            return str+str+str;
+        }
+
+    }
 }
