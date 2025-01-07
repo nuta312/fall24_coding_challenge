174c9c92c0cd977b56450e887c8059319540bc8e
Aiperi <ishenbekovaaiperi@gmail.com>
2025-01-07 18:39:53 +0600

    aiperiishenbekkyzy/warm_up2-last2
    aiperiishenbekkyzy/warm_up2-arrayCount9
    
    aiperiishenbekkyzy/warm_up2-arrayFront9
    
    aiperiishenbekkyzy/warm_up2-array123
    
    aiperiishenbekkyzy/warm_up2-stringMatch
    
    aiperiishenbekkyzy/warm_up2-stringX
    
    aiperiishenbekkyzy/warm_up2-altParis
    
    aiperiishenbekkyzy/warm_up2-stringYak
    
    aiperiishenbekkyzy/warm_up2-array667
    
    aiperiishenbekkyzy/warm_up2-noTriples
    
    aiperiishenbekkyzy/warm_up2-has271

---
+++ src/main/java/coding_bat/aiperiishenbekkyzy/warm_up2/has271.java
@@ -0,0 +1,22 @@
+package coding_bat.aiperiishenbekkyzy.warm_up2;
+
+public class has271 {
+    public boolean has271(int[] nums) {
+        int len = nums.length;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (i + 2 <= nums.length - 1) {
+
+                int j = Math.abs(nums[i] - 1);
+
+                int k = Math.abs(j - nums[i + 2]);
+
+                if (nums[i + 1] == nums[i] + 5 && k <= 2)
+
+                    return true;
+            }
+
+        }
+        return false;
+
+    }
+}
