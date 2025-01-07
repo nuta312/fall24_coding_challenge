5cab96b85516cecaf547a91c87411d646bd22639
Aliya <aliyaizabekova4@gmail.com>
2025-01-07 10:28:55 +0600

    SleepIn commit

---
+++ src/main/java/coding_bat/alia_warmup1/SleepIn.java
@@ -0,0 +1,9 @@
+package coding_bat.alia_warmup1;
+
+public class SleepIn {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation){
+            return true;
+        }return false;
+    }
+}
