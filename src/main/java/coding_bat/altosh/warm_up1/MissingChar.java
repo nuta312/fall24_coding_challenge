ccb1e975f537358a7c9e75424b5b6fc969c8cf9a
Altosh Dju <Altosh.Dju@gmai.com>
2025-01-07 23:45:20 +0600

    altosh/warm_up1-missingChar

---
+++ src/main/java/coding_bat/altosh/warm_up1/MissingChar.java
@@ -0,0 +1,11 @@
+package coding_bat.altosh.warm_up1;
+
+public class MissingChar {
+    public String missingChar(String str, int n) {
+        String front = str.substring(0,n);
+        String back = str.substring(n+1, str.length());
+        return front + back;
+
+    }
+
+}
