892df2bd2f6d5ee587a2750ce1f1843a2d53b35d
Ars472 <arsendautov6@gmail.com>
2025-01-07 20:41:58 +0600

    Delete src/main/java/coding_bat/arsen/D/warm_up1/IntMax.java

--- src/main/java/coding_bat/arsen/D/warm_up1/IntMax.java
+++
@@ -1,9 +0,0 @@
-package coding_bat.arsen.D.warm_up1;
-
-public class IntMax {
-
-    public int intMax(int a, int b, int c) {
-        int maxAB = Math.max(a, b);
-        return Math.max(maxAB, c);
-    }
-}
