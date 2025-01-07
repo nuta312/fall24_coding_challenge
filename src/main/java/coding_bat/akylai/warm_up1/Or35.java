f8d75a1d21b306bfdeb65dbebbbde84f6e70206a
Akylai <192575286+Akylai89@users.noreply.github.com>
2025-01-07 21:37:45 +0600

    Akylai/warm_up1-Or35

---
+++ src/main/java/coding_bat/akylai/warm_up1/Or35.java
@@ -0,0 +1,11 @@
+package coding_bat.akylai.warm_up1;
+
+public class Or35 {
+    public boolean or35(int n) {
+        if (n % 3 == 0 || n % 5 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
