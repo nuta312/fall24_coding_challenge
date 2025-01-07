80d50ae39e3f481a8454a95426b02415ee5ca6a1
Aidin <atyncylykov2@gmail.com>
2025-01-07 10:31:53 +0600

    aidin/warm_up1/sleepIn

--- src/main/java/coding_bat/aidin/warm_up1/sleepIn.java
+++ src/main/java/coding_bat/aidin/warm_up1/sleepIn.java
@@ -1,4 +1,12 @@
 package coding_bat.aidin.warm_up1;
 
 public class sleepIn {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation) {
+            return true;
+        }
+
+        return false;
+
+    }
 }
