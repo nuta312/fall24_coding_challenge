069ef060d6bda56488d09feccafce66ac318eb52
Aiperi <ishenbekovaaiperi@gmail.com>
2025-01-07 19:44:34 +0600

    aiperiishenbekkyzy/warm_up2-fix-icyHot

--- src/main/java/coding_bat/aiperiishenbekkyzy/warm_up1/icyHot.java
+++ src/main/java/coding_bat/aiperiishenbekkyzy/warm_up1/icyHot.java
@@ -1,4 +1,9 @@
 package coding_bat.aiperiishenbekkyzy.warm_up1;
 
 public class icyHot {
+    public boolean icyHot(int temp1, int temp2) {
+        if ((temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0))
+            return true;
+        return false;
+    }
 }
