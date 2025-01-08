9912c9006c5eb9ca0a7f48d31f4c4c858fbf18ca
Aiperi <ishenbekovaaiperi@gmail.com>
2025-01-08 13:41:18 +0600

    aiperiishenbekkyzy/string_1-cigarParty

---
+++ src/main/java/coding_bat/aiperiishenbekkyzy/logic_1/ciigarParty.java
@@ -0,0 +1,11 @@
+package coding_bat.aiperiishenbekkyzy.logic_1;
+
+public class ciigarParty {
+    public boolean cigarParty(int cigars, boolean isWeekend) {
+        if (isWeekend) {
+            return cigars >= 40;  // No upper limit on the weekend
+        } else {
+            return cigars >= 40 && cigars <= 60;  // Between 40 and 60 on weekdays
+        }
+    }
+}
