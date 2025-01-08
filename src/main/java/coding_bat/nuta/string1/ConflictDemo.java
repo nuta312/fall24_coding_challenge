fcbe36aaa156bc3806bc50ffda62dec38b4c7fc9
Nursultan <musakunovnur@gmail.com>
2025-01-08 10:54:56 +0600

    My name is Nuta commit

--- src/main/java/coding_bat/nuta/string1/ConflictDemo.java
+++ src/main/java/coding_bat/nuta/string1/ConflictDemo.java
@@ -2,6 +2,6 @@ package coding_bat.nuta.string1;
 
 public class ConflictDemo {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("My name is Nuta.");
     }
 }
