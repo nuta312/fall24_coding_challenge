57ea3453f3f24b6fa08f6d228c74713b8575e153
Nursultan <musakunovnur@gmail.com>
2025-01-08 11:04:45 +0600

    fix my tests

--- src/main/java/coding_bat/nuta/string1/ConflictDemo.java
+++ src/main/java/coding_bat/nuta/string1/ConflictDemo.java
@@ -2,6 +2,6 @@ package coding_bat.nuta.string1;
 
 public class ConflictDemo {
     public static void main(String[] args) {
-        System.out.println("Today is beautiful day");
+        System.out.println("Fix my tests");
     }
 }
