bfbdcd35f4ea3255b0cf00c4221be5554c8880f0
Aigerim Sardarbekova <aigerims@MacBook-Air-Aigerim-2.local>
2025-01-09 01:43:44 +0600

    gera/str1.7-squash7
    gera/str1.7 -squash7
    
    gera/squash1
    
    str1.7-1
    
    str1.7 -2
    
    str1.7 -3
    
    gera/str1.7 -3
    
    gera/str1.7-1
    
    str1.7-3
    
    str1.7-2.3
    
    last
    
    1.1
    
    1.2
    
    1.3

---
+++ src/main/java/coding_bat/gera/string1/SeeColor.java
@@ -0,0 +1,15 @@
+package coding_bat.gera.string1;
+
+public class SeeColor {
+    public String seeColor(String str) {
+        if (str.startsWith("red")){
+            return "red";
+        }
+        else if (str.startsWith("blue")){
+            return "blue";
+        }
+        else {
+            return "";
+        }
+    }
+}
