45a476490d72facff42fdb75795136f548f6a5bd
Nursultan <musakunovnur@gmail.com>
2025-01-08 11:05:30 +0600

    Smoke conflict

--- src/main/java/coding_bat/nuta/string1/ConflictDemo.java
+++ src/main/java/coding_bat/nuta/string1/ConflictDemo.java
@@ -2,6 +2,6 @@ package coding_bat.nuta.string1;
 
 public class ConflictDemo {
     public static void main(String[] args) {
-        System.out.println("Fix my tests");
+        System.out.println("Smoke conflict");
     }
 }
