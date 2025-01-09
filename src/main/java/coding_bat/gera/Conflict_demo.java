394a0ee69b0e974501ebc2bc8c7888283e89740c
Aigerim Sardarbekova <aigerims@MacBook-Air-Aigerim-2.local>
2025-01-09 14:22:32 +0600

    fix my test

--- src/main/java/coding_bat/gera/Conflict_demo.java
+++ src/main/java/coding_bat/gera/Conflict_demo.java
@@ -2,6 +2,6 @@ package coding_bat.gera;
 
 public class Conflict_demo {
     public static void main(String[] args) {
-        System.out.println("My name is Gera");
+        System.out.println("fix my test");
     }
 }
