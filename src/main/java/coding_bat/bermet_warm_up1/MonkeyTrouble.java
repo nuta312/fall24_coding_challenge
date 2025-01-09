af04426290ef2d96b8d0ce62cec42e9004916f63
BermetB <bermet160606@mail.com>
2025-01-09 15:22:14 +0600

    SleepIn commit

--- src/main/java/coding_bat/bermet_warm_up1/MonkeyTrouble.java
+++
@@ -1,7 +0,0 @@
-package coding_bat.bermet_warm_up1;
-
-public class MonkeyTrouble {
-    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
-        return (aSmile && bSmile) || (!aSmile && !bSmile);
-    }
-}
