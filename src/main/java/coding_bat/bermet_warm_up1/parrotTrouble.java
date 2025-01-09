ebc07856f92d63b34889d4b33c81f1e1531e6068
BermetB <bermet160606@mail.com>
2025-01-09 16:36:53 +0600

    Parrot Trouble commit

---
+++ src/main/java/coding_bat/bermet_warm_up1/parrotTrouble.java
@@ -0,0 +1,16 @@
+package coding_bat.bermet_warm_up1;
+
+public class parrotTrouble {
+    public boolean parrotTrouble(boolean talking, int hour) {
+        if (hour < 7 && hour > 20) {
+            if (hour >= 1 && hour <= 7) {
+                return true;
+            } else if (hour > 20 && hour < 24) {
+                return true;
+            }
+        } else if (hour >= 7 && hour <= 20) {
+            return false;
+        }
+        return talking;
+    }
+}
