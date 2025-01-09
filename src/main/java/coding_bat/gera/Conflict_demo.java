c44a145517c9033b791e8d274ae769c57a8ef029
Aigerim Sardarbekova <aigerims@MacBook-Air-Aigerim-2.local>
2025-01-09 14:07:01 +0600

    name commit
    # Conflicts:
    #	src/main/java/coding_bat/gera/Conflict_demo.java

--- src/main/java/coding_bat/gera/Conflict_demo.java
+++ src/main/java/coding_bat/gera/Conflict_demo.java
@@ -2,6 +2,6 @@ package coding_bat.gera;
 
 public class Conflict_demo {
     public static void main(String[] args) {
-        System.out.println("Today is a beautiful day");
+        System.out.println("My name is Gera");
     }
 }
