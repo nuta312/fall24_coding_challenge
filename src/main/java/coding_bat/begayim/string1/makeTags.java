57bb3a41f5fc26b9820fd25b5d8e3bc80c2026e5
Begayim <begayimmusakeeva@gmail.com>
2025-01-10 00:05:25 +0700

    Begayim_String1_makeTags

---
+++ src/main/java/coding_bat/begayim/string1/makeTags.java
@@ -0,0 +1,7 @@
+package coding_bat.begayim.string1;
+
+public class makeTags {
+    public String makeTags(String tag, String word) {
+        return ("<" + tag + ">" + word + "</" + tag + ">");
+    }
+}
