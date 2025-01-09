0fd8908c6411a5ddb395618d39a1a79597a30b53
Begayim <begayimmusakeeva@gmail.com>
2025-01-10 00:19:05 +0700

    Begayim_String1_HasBad

---
+++ src/main/java/coding_bat/begayim/string1/HasBad.java
@@ -0,0 +1,13 @@
+package coding_bat.begayim.string1;
+
+public class HasBad {
+    public boolean hasBad(String str) {
+        if (str.length() >= 3 && str.substring(0, 3).equals("bad")) {
+            return true;
+        }
+        if (str.length() >= 4 && str.substring(1, 4).equals("bad")) {
+            return true;
+        }
+        return false;
+    }
+}
