c4207d7c3aec313ccb9af87784f64de4695d4beb
BermetB <bermet160606@mail.com>
2025-01-09 15:30:51 +0600

    Monkey Trouble commit

---
+++ src/main/java/coding_bat/bermet_warm_up1/MonkeyTrouble.java
@@ -0,0 +1,8 @@
+package coding_bat.bermet_warm_up1;
+
+public class MonkeyTrouble {
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        return (aSmile && bSmile) || (!aSmile && !bSmile);
+
+    }
+}
