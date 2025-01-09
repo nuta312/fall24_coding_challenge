1a88ae44b0469c30a0ea1168faf72b978839e919
Aigerim Sardarbekova <aigerims@MacBook-Air-Aigerim-2.local>
2025-01-09 14:34:25 +0600

    regress fixes

--- src/main/java/coding_bat/gera/Conflict_demo.java
+++ src/main/java/coding_bat/gera/Conflict_demo.java
@@ -2,6 +2,6 @@ package coding_bat.gera;
 
 public class Conflict_demo {
     public static void main(String[] args) {
-        System.out.println("fix my test");
+        System.out.println("Regress fixes");
     }
 }
