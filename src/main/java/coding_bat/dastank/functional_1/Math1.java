29f9f5927c7072664a19a464ffca9089cd63a58b
DastanK <dastan.keneshbekov@gmail.ru>
2025-01-09 13:49:19 +0600

    functional_1

---
+++ src/main/java/coding_bat/dastank/functional_1/Math1.java
@@ -0,0 +1,10 @@
+package coding_bat.dastank.functional_1;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Math1 {
+    public List<Integer> math1(List<Integer> nums) {
+        return nums.stream().map(n->(n+1)*10).collect(Collectors.toList());
+    }
+}
