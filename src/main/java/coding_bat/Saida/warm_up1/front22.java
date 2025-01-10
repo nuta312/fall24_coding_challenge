04762dde348b538729df3ba7dce743ec5bcf6f46
Saida77R <Saida77R@users.noreply.github.com>
2025-01-11 00:33:06 +0600

    Saida/warm_up1/front22

---
+++ src/main/java/coding_bat/Saida/warm_up1/front22.java
@@ -0,0 +1,10 @@
+package coding_bat.Saida.warm_up1;
+
+public class front22 {
+    public String front22(String str) {
+        String front = str.length() < 2 ?
+                str: str.substring(0, 2);
+        return front + str + front;
+
+    }
+}
