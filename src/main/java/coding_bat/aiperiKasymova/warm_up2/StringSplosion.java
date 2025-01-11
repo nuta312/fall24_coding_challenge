bfce64fbe0c892807face6cb58631fa23b64bb52
Aiperi <aibige24wka@gmail.com>
2025-01-11 19:02:35 +0600

    Warm_up2 commits# This is a combination of 17 commits.
    aiperiKasymova/warm_up2/FrontTimes
    
    aiperiKasymova/warm_up2/CountXX
    
    aiperiKasymova/warm_up2/DoubleX
    
    aiperiKasymova/warm_up2/StringBits
    
    aiperiKasymova/warm_up2/StringBits
    
    aiperiKasymova/warm_up2/StringSplosion
    
    aiperiKasymova/warm_up2/Last2
    
    aiperiKasymova/warm_up2/ArrayCount9
    
    aiperiKasymova/warm_up2/ArrayFront9
    
    aiperiKasymova/warm_up2/Array123
    
    aiperiKasymova/warm_up2/StringMatch
    
    aiperiKasymova/warm_up2/StringX
    
    aiperiKasymova/warm_up2/AltPairs
    
    aiperiKasymova/warm_up2/StringYak
    
    aiperiKasymova/warm_up2/Array667
    
    aiperiKasymova/warm_up2/NoTriples
    
    aiperiKasymova/warm_up2/Has271

---
+++ src/main/java/coding_bat/aiperiKasymova/warm_up2/StringSplosion.java
@@ -0,0 +1,11 @@
+package coding_bat.aiperiKasymova.warm_up2;
+
+public class StringSplosion {
+    public String stringSplosion(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            sb.append(str.substring(0, i + 1));
+        }
+        return sb.toString();
+    }
+}
