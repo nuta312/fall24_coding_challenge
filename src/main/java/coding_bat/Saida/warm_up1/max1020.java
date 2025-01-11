483cff50b8cac4a7f9a6cdb93d47c5d2f26993d4
Saida77R <saida.rust77@gmail.com>
2025-01-12 01:10:04 +0600

    Saida/warm_up1/max1020

---
+++ src/main/java/coding_bat/Saida/warm_up1/max1020.java
@@ -0,0 +1,15 @@
+package coding_bat.Saida.warm_up1;
+
+public class max1020 {
+    public int max1020(int a, int b) {
+        if (a >= 10 && a <= 20 && b >= 10 && b <= 20) {
+            return Math.max(a, b);
+        } else if (a >= 10 && a <= 20) {
+            return a;
+        } else if (b >= 10 && b <= 20) {
+            return b;
+        } else {
+            return 0;
+        }
+    }
+}
