4b9d756c4cc3b3730c001680196d54eb41a7ef39
Rinat Zhanaliev <rinatjanaliev@MacBook-Pro-Rinat.local>
2025-01-11 23:42:36 +0600

    rinat/String-1:extraEnd

---
+++ src/main/java/coding_bat/rinat/String1/ExtraEnd.java
@@ -0,0 +1,7 @@
+package coding_bat.rinat.String1;
+
+public class ExtraEnd {
+    public String extraEnd(String str) {
+        return   str.substring(str.length()-2)+ str.substring(str.length()-2)+ str.substring(str.length()-2);
+    }
+}
