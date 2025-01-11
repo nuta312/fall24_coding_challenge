c899e0cac0498b3d6209b3353ccbcc98e3fbbf6a
Rinat Zhanaliev <rinatjanaliev@MacBook-Pro-Rinat.local>
2025-01-11 23:33:26 +0600

    rinat/String-1:makeTags

---
+++ src/main/java/coding_bat/rinat/String1/MakeTags.java
@@ -0,0 +1,8 @@
+package coding_bat.rinat.String1;
+
+public class MakeTags {
+    public String makeTags(String tag, String word) {
+        return "<"+ tag + ">" + word + "</" + tag + ">";
+    }
+
+}
