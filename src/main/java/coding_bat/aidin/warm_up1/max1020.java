ea97df81e41feae945d931a45d3f14c931486b81
Aidin <atyncylykov2@gmail.com>
2025-01-11 19:22:57 +0600

    Aidin/warm_up1/max1020

---
+++ src/main/java/coding_bat/aidin/warm_up1/max1020.java
@@ -0,0 +1,23 @@
+package coding_bat.aidin.warm_up1;
+
+public class max1020 {
+    public int max1020(int a, int b) {
+        int tempa;
+        int tempb;
+
+        if (a >= 10 && a <= 20) {
+            tempa = a;
+        } else {
+            tempa = 0;
+        }
+
+        if (b >= 10 && b <= 20) {
+            tempb = b;
+        } else {
+            tempb = 0;
+        }
+
+        return Math.max(tempa, tempb);
+    }
+
+}
