9e8272645338c19153b6d58a8a6a6b48d8991cd4
adastanious <dastanaidarov1@gmail.com>
2025-01-12 12:03:38 +0600

    amend-dastanA-warm-up-2-CountXX

--- src/main/java/coding_bat/dastanA/warm_up2/CountXX.java
+++ src/main/java/coding_bat/dastanA/warm_up2/CountXX.java
@@ -2,32 +2,12 @@ package coding_bat.dastanA.warm_up2;
 
 public class CountXX {
     int countXX(String str) {
-        String[] words = str.split(" ");
-        if (words.length > 1) {
-            int count = 0;
-            for (int i = 0; i < words.length; i++) {
-                for (int j = 0; j < words[i].length(); j++) {
-                    if (words[i].charAt(j) == 'x') {
-                        count++;
-                    }
-                } count -= 1;
-            }
-            if (count <= 0) {
-                count = 0;
-            }
-            return count;
-        }
-
-        int count1 = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == 'x') {
-                count1++;
+        int count = 0;
+        for(int i = 0; i < str.length()-1; i++) {
+            if (str.charAt(i) == 'x' && str.charAt(i+1) == 'x') {
+                count++;
             }
         }
-        if (count1 <= 0) {
-            return count1 = 0;
-        }
-        return count1 - 1;
+        return count;
     }
-
 }
