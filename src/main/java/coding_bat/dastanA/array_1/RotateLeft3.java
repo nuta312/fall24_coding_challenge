85c9bb24354c86936a3195d42b575d28c5e30c68
adastanious <dastanaidarov1@gmail.com>
2025-01-12 23:06:45 +0600

    dastanA-array-1-RotateLeft3

---
+++ src/main/java/coding_bat/dastanA/array_1/RotateLeft3.java
@@ -0,0 +1,8 @@
+package coding_bat.dastanA.array_1;
+
+public class RotateLeft3 {
+    public int[] rotateLeft3(int[] nums) {
+        int[] nums2 = {nums[1],nums[2],nums[0]};
+        return nums2;
+    }
+}
