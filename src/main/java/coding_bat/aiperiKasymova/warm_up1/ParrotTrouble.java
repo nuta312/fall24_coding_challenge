11876660b9c7e88e2bd6bb3c9b165be6c6634c4b
Aigerim Sardarbekova <aigerims@MacBook-Air-Aigerim-2.local>
2025-01-12 19:25:24 +0600

    gera/lambda_demo

--- src/main/java/coding_bat/aiperiKasymova/warm_up1/ParrotTrouble.java
+++ src/main/java/coding_bat/aiperiKasymova/warm_up1/ParrotTrouble.java
@@ -1,8 +1,10 @@
 package coding_bat.aiperiKasymova.warm_up1;
+
 public class ParrotTrouble {
-   if(talking && (hour < 7 || hour > 20))
-    return true;
-    
-    return false;
-}
+    public boolean parrotTrouble(boolean talking, int hour) {
+        if (talking && (hour < 7 || hour > 20))
+            return true;
+
+        return false;
+    }
 }
---
+++ src/main/java/coding_bat/gera/lamda_demo/MathOperation.java
@@ -0,0 +1,8 @@
+package coding_bat.gera.lamda_demo;
+
+// это интерфейс, который содержит только один абстрактный метод
+
+@FunctionalInterface
+interface MathOperation {
+    int operation (int a, int b);
+}
