3015dbdde137dd03b67180d258f457eb8a55128a
Aliya <aliyaizabekova4@gmail.com>
2025-01-12 22:53:50 +0600

    Regress fixes

--- src/main/java/coding_bat/alia_warmup1/ConflictDemo.java
+++ src/main/java/coding_bat/alia_warmup1/ConflictDemo.java
@@ -2,6 +2,6 @@ package coding_bat.alia_warmup1;
 
 public class ConflictDemo {
     public static void main(String[] args) {
-        System.out.println("Fix my tests");
+        System.out.println("Regress fixes");
     }
 }
