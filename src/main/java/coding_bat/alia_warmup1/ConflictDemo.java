6720cbf661fa82602bd3d2bc1644f9ddc92dee76
Aliya <aliyaizabekova4@gmail.com>
2025-01-12 22:00:36 +0600

    Beautiful day

--- src/main/java/coding_bat/alia_warmup1/ConflictDemo.java
+++ src/main/java/coding_bat/alia_warmup1/ConflictDemo.java
@@ -2,6 +2,6 @@ package coding_bat.alia_warmup1;
 
 public class ConflictDemo {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Today is a beautiful day!");
     }
 }
