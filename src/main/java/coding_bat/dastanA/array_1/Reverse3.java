885551fc6a0de5234d3f480e19747a04096a85a0
adastanious <dastanaidarov1@gmail.com>
2025-01-12 23:08:52 +0600

    dastanA-array-1-Reverse3

---
+++ src/main/java/coding_bat/dastanA/array_1/Reverse3.java
@@ -0,0 +1,8 @@
+package coding_bat.dastanA.array_1;
+
+public class Reverse3 {
+    public int[] reverse3(int[] nums) {
+        int [] numbers = {nums[2], nums[1], nums[0]};
+        return numbers;
+    }
+}
