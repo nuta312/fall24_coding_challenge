7cf1396147b08fed983b24ebdcb6cd68b2b112fb
MvratPro <murat.jukenoff@gmail.com>
2025-01-12 20:39:30 -0500

    BackAround

---
+++ src/main/java/coding_bat/Murat/Warmup1/BackAround.java
@@ -0,0 +1,7 @@
+package coding_bat.Murat.Warmup1;
+
+public class BackAround {
+    public String backAround(String str) {
+        return str.charAt(str.length() - 1) + str + str.charAt(str.length() - 1);
+    }
+}
