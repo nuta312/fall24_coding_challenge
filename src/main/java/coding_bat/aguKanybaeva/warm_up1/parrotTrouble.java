995989c2959076eb2e88ea989f60cc7850ea37f3
Agu <kanvolvaak94@gmail.com>
2025-01-13 11:17:36 +0600

    parrotTrouble

---
+++ src/main/java/coding_bat/aguKanybaeva/warm_up1/parrotTrouble.java
@@ -0,0 +1,7 @@
+package coding_bat.aguKanybaeva.warm_up1;
+
+public class parrotTrouble {
+    public boolean parrotTrouble(boolean talking, int hour) {
+        return talking && (hour < 7 || hour > 20);
+    }
+}
