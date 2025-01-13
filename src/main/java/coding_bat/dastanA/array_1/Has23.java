51cb0c7ec5a1d91248c6f0bcd7a2237381c476a1
adastanious <dastanaidarov1@gmail.com>
2025-01-13 10:01:10 +0600

    dastanA-array-1-Has23

---
+++ src/main/java/coding_bat/dastanA/array_1/Has23.java
@@ -0,0 +1,7 @@
+package coding_bat.dastanA.array_1;
+
+public class Has23 {
+    public boolean has23(int[] nums) {
+        return (nums[0] == 2 || nums[1] == 2 || nums[0] == 3 || nums[1] == 3);
+    }
+}
