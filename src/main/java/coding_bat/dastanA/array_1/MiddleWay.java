185b42928df4deac66d3d26c11d5e16c9e0c8768
adastanious <dastanaidarov1@gmail.com>
2025-01-13 09:03:52 +0600

    dastanA-array-1-MiddleWay

---
+++ src/main/java/coding_bat/dastanA/array_1/MiddleWay.java
@@ -0,0 +1,10 @@
+package coding_bat.dastanA.array_1;
+
+public class MiddleWay {
+    public int[] middleWay(int[] a, int[] b) {
+        int [] nums = new int[2];
+        nums[0] = a[1];
+        nums[1] = b[1];
+        return nums;
+    }
+}
