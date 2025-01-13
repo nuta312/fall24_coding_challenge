c3ae371fedbdef039351d9b5eece74b0dc54ab0b
Agu <kanvolvaak94@gmail.com>
2025-01-13 11:12:24 +0600

    diff21

---
+++ src/main/java/coding_bat/aguKanybaeva/warm_up1/diff21.java
@@ -0,0 +1,10 @@
+package coding_bat.aguKanybaeva.warm_up1;
+
+public class diff21 {
+    public int diff21(int n) {
+        if(n <= 21)
+            return 21 - n;
+
+        return 2 * (n - 21);
+    }
+}
