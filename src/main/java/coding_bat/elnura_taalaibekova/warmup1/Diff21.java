d0cf8b69933e8d4dff14d9d26d715d0605b3c9ec
Your Name <you@example.com>
2025-01-18 20:07:36 +0600

    elnura/warmup1/1-3tasks

---
+++ src/main/java/coding_bat/elnura_taalaibekova/warmup1/Diff21.java
@@ -0,0 +1,12 @@
+package coding_bat.elnura_taalaibekova.warmup1;
+
+public class Diff21 {
+    public int diff21(int n) {
+        if (n <= 21) {
+            return 21 - n;
+        } else {
+            return (n - 21) * 2;
+        }
+
+    }
+}
