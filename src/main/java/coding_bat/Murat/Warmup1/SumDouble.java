6cb84c739f0353a3bd4ea82566eff12956bd74f1
MvratPro <murat.jukenoff@gmail.com>
2025-01-19 14:40:18 -0500

    CodingBat 6 topics, 10 problems each

--- src/main/java/coding_bat/Murat/Warmup1/SumDouble.java
+++ src/main/java/coding_bat/Murat/Warmup1/SumDouble.java
@@ -1,4 +1,9 @@
 package coding_bat.Murat.Warmup1;
 
 public class SumDouble {
+    public int sumDouble(int a, int b) {
+        if (a != b) return a + b;
+        else if (a == b) return (a + b) * 2;
+        return 0;
+    }
 }
